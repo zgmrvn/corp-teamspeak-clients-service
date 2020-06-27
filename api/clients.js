39472bd7ca6d16d727c7e021794f88f2b409622b
zgmrvn <zgmrvn@gmail.com>
2020-06-27 21:07:33 +0200

    Refactor to variables instead of redis

--- api/clients.js
+++ api/clients.js
@@ -1,29 +1,13 @@
-const redis = require('redis')
-const { promisify } = require('util')
 const { queryTeamspeak } = require('../lib/ts')
 
-const redisOptions = { db: 0, password: process.env.REDIS_PASSWORD }
+let clients = []
+let updatedAt = 0
 
 export default async (req, res) => {
-  const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, redisOptions)
-  const existsAsync = promisify(client.exists).bind(client)
-  const lrangeAsync = promisify(client.lrange).bind(client)
-  const rpushAsync = promisify(client.rpush).bind(client)
-  const expireAsync = promisify(client.expire).bind(client)
-
-  let clients = []
-
-  const exists = await existsAsync('clients')
-
-  if (exists) {
-    clients = await lrangeAsync('clients', 0, -1)
-  } else {
-    clients = await queryTeamspeak()
-    
-    await rpushAsync('clients', clients)
-    await expireAsync('clients', 60)
+  if (Date.now() - updatedAt > 60 * 1000) {
+    clients = clients = await queryTeamspeak()
+    updatedAt = Date.now()
   }
 
-  client.end(true)
-  res.json(clients)
+  res.json({ clients, updatedAt })
 }
